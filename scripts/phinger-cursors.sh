5bec5f8dbc8a724bcfaf9e864b48be2b5fa6efb5
areif-dev <aj@ajreifsnyder.com>
2025-05-21 20:30:45 -0400

    laptop: make sure that ~/Downloads is created when installing phinger cursor

--- scripts/phinger-cursors.sh
+++ scripts/phinger-cursors.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash 
 
+mkdir ~/Downloads
 cd ~/Downloads
 curl -Lo phinger.tar.bz2 "https://github.com/phisch/phinger-cursors/releases/download/v2.1/phinger-cursors-variants.tar.bz2"
 tar -xf ~/Downloads/phinger.tar.bz2
