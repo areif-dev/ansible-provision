58abcd42407b2a73a9cae4724eaa426d7b6e41cc
areif-dev <aj@ajreifsnyder.com>
2025-08-15 00:57:16 -0400

    remove unnecessary call to sed in rofi script

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -5,7 +5,7 @@ mkdir -p /tmp/extras
 set -ouex pipefail 
 
 echo "Installing Rofi"
-ROFI_VERSION=$(curl -s "https://api.github.com/repos/davatorium/rofi/releases/latest" | jq -r '.tag_name' | sed 's/v//')
+ROFI_VERSION=$(curl -s "https://api.github.com/repos/davatorium/rofi/releases/latest" | jq -r '.tag_name')
 curl -L -o "/tmp/extras/rofi-${ROFI_VERSION}.tar.gz" "https://github.com/davatorium/rofi/releases/download/v${ROFI_VERSION}/rofi-${ROFI_VERSION}.tar.gz"
 tar -xzf "/tmp/extras/rofi-${ROFI_VERSION}.tar.gz" -C "/tmp/extras"
 mv /tmp/extras/rofi /usr/bin
